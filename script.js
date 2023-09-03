4ac7dd97bb72315df840197bbbffeb812f615443
Ankitfactech <98767716+Ankitfactech@users.noreply.github.com>
2023-09-04 02:31:03 +0530

    Hello

--- script.js
+++ script.js
@@ -4,3 +4,13 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+const audio = new Audio("WhatsApp Audio 2023-09-04 at 00.04.49.mp4");
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach(button => {
+button.addEventListener("click", () => {
+audio.play();
+});
+});
+
